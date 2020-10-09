94c714719ee47f3a13d22602349c34bdce244b11
Jah <kennedye2@vcu.edu>
2020-10-08 21:43:07 -0400

    the toggle works nowgit add --all

--- public/main.js
+++ public/main.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("done");
+    var clicked = false;
+    var lastClicked = "";
+    let someString = "This is a project I created because I was getting really tired of opening up photoshop every time I wanted to change something.";
+    let target = document.getElementById("1");
+    let output = document.getElementById("text-display");
+    target.addEventListener("mouseenter", function( event ) {
+        output.innerHTML = someString;
+    })
+    target.addEventListener("mouseout", function( event ) {
+        output.innerHTML = lastClicked;
+    })
+    target.addEventListener("click", function( event ) {
+        if (clicked == false){
+            output.innerHTML = someString;
+            lastClicked = someString;
+        } else {
+            output.innerHTML = "";
+            lastClicked = "";
+        }
+        clicked = !clicked;
+
+
+/*         tog[1] == !tog[1];
+        if (tog[1] == false){
+            output.innerHTML = someString;
+            lastClicked = someString;
+        } else {
+            output.innerHTML = "";
+            lastClicked = "";
+        } */
+    })
 })
 
 /* let test = document.getElementById("test");
