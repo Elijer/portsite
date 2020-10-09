f0226e8f399eef7dfc615957f1783cc12431e918
Jah <kennedye2@vcu.edu>
2020-10-08 21:57:01 -0400

    iterating through the li elements successfully

--- public/main.js
+++ public/main.js
@@ -1,39 +1,46 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var clicked = false;
+    var activeClick = false;
     var lastClicked = "";
-    let someString = "This is a project I created because I was getting really tired of opening up photoshop every time I wanted to change something.";
-    let target = document.getElementById("1");
+    let strings = [
+        "Color Picka",
+        "Stripe",
+        "Unes",
+        "Video",
+        "Amination",
+        "oysters (interview)",
+        "say hi",
+        "mwesy",
+        "rez"
+    ];
+
+    var chillun = document.getElementById("the-list").childNodes;
+    console.log(chillun[1].innerHTML);
+
+    for (var i = 0; i < strings.length; i++){
+        //console.log(strings[i]);
+    }
+})
+
+/* function assignHoverEvent(target){
+    let target = document.getElementById(target);
     let output = document.getElementById("text-display");
+
     target.addEventListener("mouseenter", function( event ) {
         output.innerHTML = someString;
     })
+
     target.addEventListener("mouseout", function( event ) {
         output.innerHTML = lastClicked;
     })
+
     target.addEventListener("click", function( event ) {
-        if (clicked == false){
+        if (activeItem == false){
             output.innerHTML = someString;
             lastClicked = someString;
         } else {
             output.innerHTML = "";
             lastClicked = "";
         }
-        clicked = !clicked;
-
-
-/*         tog[1] == !tog[1];
-        if (tog[1] == false){
-            output.innerHTML = someString;
-            lastClicked = someString;
-        } else {
-            output.innerHTML = "";
-            lastClicked = "";
-        } */
+        activeItem = !activeItem;
     })
-})
-
-/* let test = document.getElementById("test");
-  
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-test.addEventListener("mouseenter", function( event ) {  */
+} */
