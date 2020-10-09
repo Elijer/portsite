3dabf5c57c2fd7c33db8457888581b4619475bf2
Jah <kennedye2@vcu.edu>
2020-10-08 21:24:00 -0400

    added a big clunky rectangle

---
+++ public/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("done");
+})
+
+/* let test = document.getElementById("test");
+  
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function( event ) {  */
