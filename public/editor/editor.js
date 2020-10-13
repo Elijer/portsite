66df72cc642823e7707caaaeefe5b2958cf8a642
Jah <kennedye2@vcu.edu>
2020-10-13 18:29:18 -0400

    cursor styling, still too much space though

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -5,32 +5,74 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var phantom = document.getElementById("phantom");
 
     phantom.addEventListener('input', typing);
+
+    var f = document.getElementById("cursor");
+
+/*     setInterval(function() {
+        f.style.display = (f.style.display == 'none' ? '' : 'none');
+    }, 100) */
+
+    cursorBlink();
+
+    document.querySelector('#phantom').addEventListener('keypress', exception);
+
+    state = {
+        typing: false
+    }
 })
 
 var typeWriter = function(){
     console.log("ding!");
 
-    var page = document.getElementById("page");
+    var entry = document.getElementById("entry");
     var phantom = document.getElementById("phantom");
     phantom.focus();
-    page.innerHTML = `Donkey!!`;
+    entry.innerHTML = "";
 
 }
 
 var typing = function(e){
 
-    var page = document.getElementById("page");
-    var currentText = page.innerHTML
+    document.getElementById("cursor").style.display = "inline";
+    
+    state.typing = true;
+
+    var entry = document.getElementById("entry");
+    var currentText = entry.innerHTML
+    //console.log(e);
 
     if (e.inputType === "insertText"){
         console.log(e.target.value)
-        page.innerHTML = currentText + e.data;
+        entry.innerHTML = currentText + e.data;
     } else if (e.inputType === "deleteContentBackward"){
-        page.innerHTML = currentText.substring(0, currentText.length - 1);
+        entry.innerHTML = currentText.substring(0, currentText.length - 1);
     } else {
         console.log("Unknown input type: ", e.inputType);
     }
 }
+
+var exception = function(e){
+        if (e.key === 'Enter') {
+            console.log("well hey");
+            var entry = document.getElementById("entry");
+            var currentText = entry.innerHTML;
+            entry.innerHTML = currentText + `</br>`
+
+        }
+}
+
+var cursorBlink = function(){
+    var cursor = document.getElementById("cursor");
+    setInterval(function(){
+        if (cursor.style.visibility == "hidden"){
+            console.log("visible")
+            cursor.style.visibility = "visible";
+        } else {
+            console.log("hidden")
+            cursor.style.visibility = "hidden";
+        }
+    }, 500);
+};
 
 /* var assignHoverEvent = function(list, i){
     output = document.getElementById("text-display");
