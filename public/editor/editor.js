cd019a58b33b08a3c61802bc830c011bc9b599f7
Jah <kennedye2@vcu.edu>
2020-10-13 17:11:34 -0400

    added javascript file with test function

---
+++ public/editor/editor.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    string = ``;
+
+})
+
+var typeWriter = function(){
+    console.log("ding!");
+}
+
+/* var assignHoverEvent = function(list, i){
+    output = document.getElementById("text-display");
+    let current = list[i];
+    let blurb = strings[i];
+
+    current.addEventListener("click", function( event ) { 
+
+        if (current.classList.contains("test")) {
+            current.classList.remove("test");
+            output.innerHTML = "";
+        } else {
+
+            list.forEach(function(el) {
+                //element.classList.add("test");
+                if (el.classList.contains("test")) {
+                    el.classList.remove("test");
+                }
+            });
+
+            current.classList.add("test");
+            output.innerHTML = blurb;
+
+        }
+
+
+    })
+} */
