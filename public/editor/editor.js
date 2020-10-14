377660e95ac1e1649850eacbfee1abc5c38e3f0f
Jah <kennedye2@vcu.edu>
2020-10-13 23:03:14 -0400

    separating into entry and outry but the problem is saving them both

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -14,15 +14,29 @@ var whereFocus = function(){
 
     if (document.activeElement === phantom){
         phantom.addEventListener('input', typing);
-        cursorBlink();
-        //moveCursor();
+        document.getElementById("cursor").style.visibility = "visible";
+        //cursorBlink();
     } else {
         phantom.removeEventListener('input', typing);
-
+        document.getElementById("cursor").style.visibility = "hidden";
     }
 }
 
 var typeWriter = function(){
+
+    console.log(i);
+    // Get Selection (or just the click I guess)
+    var s = window.getSelection();
+    var i = s.anchorOffset; // This is which character is clicked
+    var current = document.getElementById("entry").innerHTML;
+    document.getElementById("entry").innerHTML = current.substring(0, i);
+    console.log(current.substring(0, i));
+    //document.getElementById("outry").innerHTML = current.substring(i, txt.length);
+
+
+
+
+    console.log(i);
 
     var entry = document.getElementById("entry");
     var phantom = document.getElementById("phantom");
@@ -31,6 +45,35 @@ var typeWriter = function(){
     entry.innerHTML = txt;
 
 }
+
+var splice = function(str, insert, i){
+
+    var thing;
+    var prefix = str.substring(0, i);
+    var suffix = str.substring(i, str.length);
+    thing = prefix + insert + suffix;
+    return thing;
+
+}
+
+
+/* var moveCursor = function(e){
+
+    if (document.getElementById("cursor")){
+        document.getElementById("cursor").remove();
+    }
+
+    // Get Selection (or just the click I guess)
+    var s = window.getSelection();
+    var i = s.anchorOffset; // This is which character is clicked
+    console.log(i);
+
+    var cursor = `<span id = "cursor" ></span>`
+
+    txt = splice(txt, cursor, i)
+    document.getElementById("entry").innerHTML = txt;
+
+} */
 
 var typing = function(e){
 
@@ -76,7 +119,8 @@ var exception = function(e){
         }
 }
 
-var cursorBlink = function(){
+// This is a bit too much to handle for now.
+/* var cursorBlink = function(){
 
     var cursor = document.getElementById("cursor");
     cursor.style.display = "inline";
@@ -90,32 +134,4 @@ var cursorBlink = function(){
         }
     }, 800);
 
-};
-
-var moveCursor = function(e){
-
-    if (document.getElementById("cursor")){
-        document.getElementById("cursor").remove();
-    }
-
-    // Get Selection (or just the click I guess)
-    var s = window.getSelection();
-    var i = s.anchorOffset; // This is which character is clicked
-    console.log(i);
-
-    var cursor = `<div id = "cursor" ></div>`
-
-    txt = splice(txt, cursor, i)
-    document.getElementById("entry").innerHTML = txt;
-
-}
-
-var splice = function(str, insert, i){
-
-    var thing;
-    var prefix = str.substring(0, i);
-    var suffix = str.substring(i, str.length);
-    thing = prefix + insert + suffix;
-    return thing;
-
-}
+}; */
