177db0189005cc555bf30761898c3bac72fe4fc0
Jah <kennedye2@vcu.edu>
2020-10-18 16:33:29 -0400

    +1 on char iterator for ArrowDown

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -232,7 +232,7 @@ var arrowNav = function(){
                 lines = [];
                 var height = entry.offsetHeight;
 
-                for (var i = 1; i < o.length; i++){
+                for (var i = 1; i < o.length+1; i++){
 
                     // copy first character of outry
                     var take = outry.innerHTML.substring(0, 1);
