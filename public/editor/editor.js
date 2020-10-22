9e40fb41986b3cd020717a3f1fb3ea5ac9aa0ba7
Jah <kennedye2@vcu.edu>
2020-10-22 16:10:55 -0400

    fixed edge case for ArrowDown at last index at end of block

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -364,11 +364,31 @@ var arrowNav = function(){
                         page.insertBefore(next, tw);
                         next.innerHTML = total;
 
-                        var indexFromLineBeginning = e.length - currentLineBeginIndex;
+                        // Edge case: happens when at very last position;
+                        if (!currentLineBeginIndex){
 
-                        entry.innerHTML = nextText.substring(0, indexFromLineBeginning);
-                        outry.innerHTML = nextText.substring(indexFromLineBeginning, nextText.length);
+                            entry.innerHTML = "";
+                            outry.innerHTML = nextText;
+                            var height = entry.offsetHeight;
 
+                            for (var i = 0; i < nextText.length; i++){
+                                var take = outry.innerHTML.substring(0, 1);
+                                entry.innerHTML = entry.innerHTML + take;
+                                outry.innerHTML = outry.innerHTML.substring(1);
+
+                                if (entry.offsetHeight > height){
+                                    entry.innerHTML = nextText.substring(0, i-1);
+                                    outry.innerHTML = nextText.substring(i-1, nextText.length);
+                                    break;
+                                }
+                            }
+                            
+                        } else {
+                            var indexFromLineBeginning = e.length - currentLineBeginIndex;
+
+                            entry.innerHTML = nextText.substring(0, indexFromLineBeginning);
+                            outry.innerHTML = nextText.substring(indexFromLineBeginning, nextText.length);
+                        }
                     }
 
                 } else {
